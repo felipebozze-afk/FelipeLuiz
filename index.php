ff0a9485b9e1c981c82cf56ed67521200d9f3664
felipebozze-afk <felipe.bozze@aluno.senai.br>
2026-09-21 15:22:14 -0300

    atualização 2.0

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <title>Atividades PHP</title>
 </head>
 <body>
-    <a href="idade.php">verificador de idade</a>
-    <a href="notas.php">notas</a>
-    <a href="notas-get.php">notas desafio</a>
+    <div class="container">
+        <h1>Atividades PHP</h1>
+        <p><a href="idade.php">Verificador de idade</a></p>
+        <p><a href="notas.php">Situação do aluno (POST)</a></p>
+        <p><a href="notas-get.php">Situação do aluno (GET)</a></p>
+    </div>
 </body>
 </html>
