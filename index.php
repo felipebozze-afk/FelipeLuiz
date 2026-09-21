3f60d5439692d1a1a8276e2796ad423513b53505
felipebozze-afk <felipe.bozze@aluno.senai.br>
2026-09-21 14:34:46 -0300

    atualização
    atualização

--- index.php
+++ index.php
@@ -1,24 +1,69 @@
+ <?php
+        $nome = "";
+        $idade = "";
+        $resultado = "";
+
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nome = $_POST["nome"];
+            $idade = $_POST["idade"];
+        
+        }
+        if ($idade >=18)
+            {
+                $resultado = "você é maior de idade";
+            }
+
+             else if($idade <= 0)
+            {
+                $resultado = "invalido";
+            }
+
+            else {
+                $resultado ="você é menor de idade";
+            }
+
+           
+
+    ?>
+       <?php
+         $resultado != "" {
+            <?php echo "<h2>Olá, $nome! $resultado</h2>"; ?>
+         }
+           
+         ?>
+        
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+
+    <title>AULA LUCAS LUZ</title>
 </head>
+
 <body>
-    <?php
-     $nome = "felipe";
-     $idade = 37;
-?>
+         <div class='container'>
 
-<h1> nome: <?=  $nome ?>   </h1>
-<p>idade: <?= $idade ?> </p>
+         <h1>Informações do Aluno</h1>
 
-<?php if ($idade >= 18) { ?>
-    <p>Maior de idade</p>
-<?php } else { ?>
-    <p>Menor de idade</p>
-<?php } ?>
+         <form method="POST">
+
+             <input type="text" id="nome" name="nome" required placeholder="Digite seu nome">
+
+             <input type="number" id="idade" name="idade" required placeholder="Digite sua idade">
+
+             <button type="submit">Verificar</button>
+
+
+         </form>
+
+         </div>
+
+         
 
 </body>
+
 </html>
