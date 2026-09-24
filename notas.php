dd0919f12060b89655d0c375c518c2b302546823
felipebozze-afk <felipe.bozze@aluno.senai.br>
2026-09-24 15:11:26 -0300

    att

--- notas.php
+++ notas.php
@@ -62,7 +62,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="index.css?v=2">
     <title>Situação do Aluno</title>
 </head>
 <body>
